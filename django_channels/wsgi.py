0c9a7abf8ba8916e35f5ee148963ba46fda2e863
fargusplumdoodle <fargusplumdoodle@gmail.com>
2020-02-22 20:33:43 -0800

    removed venv

--- django_channels/wsgi.py
+++ django_channels/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_channels.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_channels.settings")
 
 application = get_wsgi_application()
