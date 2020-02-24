ac8824f6c716c02ea4b56c7ae80722f7ce92ab20
fargusplumdoodle <fargusplumdoodle@gmail.com>
2020-02-23 19:49:05 -0800

    finished tutorial part 2

--- django_channels/routing.py
+++ django_channels/routing.py
@@ -1,3 +1,12 @@
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
-application = ProtocolTypeRouter({})
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
+
